ff5be384ad85a362c8082d9e34945721c8e26d8b
ThomasBeder <82496414+ThomasBeder@users.noreply.github.com>
2025-06-14 15:08:50 +0200

    Update allcatchr_1.1.R

--- R/allcatchr_1.1.R
+++ R/allcatchr_1.1.R
@@ -883,16 +883,16 @@ allcatchr_1.1 <- function(Lineage = "B-ALL", Counts.file=NA, ID_class="symbol", 
     TALL_subtype_peds$`T-ALL sub-cluster candidate`[-pos1] <- gsub("C1.2 (LMO2 γδ-like)", "", fixed = T, TALL_subtype_peds$`T-ALL sub-cluster candidate`[-pos1])
     
      # this is new from here
-     TALL_subtype_peds$`C1.1 (TAL1 αβ-like)`[-pos1] <- NA
-     TALL_subtype_peds$`C1.2 (LMO2 γδ-like)`[-pos1] <- NA
+     #TALL_subtype_peds$`C1.1 (TAL1 αβ-like)`[-pos1] <- NA
+     #TALL_subtype_peds$`C1.2 (LMO2 γδ-like)`[-pos1] <- NA
     
     } else {
       TALL_subtype_peds$`T-ALL sub-cluster candidate` <- gsub("C1.1 (TAL1 αβ-like)", "", fixed = T, TALL_subtype_peds$`T-ALL sub-cluster candidate`)
       TALL_subtype_peds$`T-ALL sub-cluster candidate` <- gsub("C1.2 (LMO2 γδ-like)", "", fixed = T, TALL_subtype_peds$`T-ALL sub-cluster candidate`)
 
-     # this is new from here
-     TALL_subtype_peds$`C1.1 (TAL1 αβ-like)` <- NA
-     TALL_subtype_peds$`C1.2 (LMO2 γδ-like)` <- NA
+     ## this is new from here
+     #TALL_subtype_peds$`C1.1 (TAL1 αβ-like)` <- NA
+     #TALL_subtype_peds$`C1.2 (LMO2 γδ-like)` <- NA
       
     }
     
@@ -904,9 +904,9 @@ allcatchr_1.1 <- function(Lineage = "B-ALL", Counts.file=NA, ID_class="symbol", 
     TALL_subtype_peds$`T-ALL sub-cluster candidate`[-pos2] <- gsub("C12.3 (immature other)", "", fixed = T, TALL_subtype_peds$`T-ALL sub-cluster candidate`[-pos2])
   
     # this is new from here
-     TALL_subtype_peds$`C12.1 (MED12)`[-pos2] <- NA
-     TALL_subtype_peds$`C12.2 (HOXA13)`[-pos2] <- NA
-     TALL_subtype_peds$`C12.3 (immature other)`[-pos2] <- NA
+   #  TALL_subtype_peds$`C12.1 (MED12)`[-pos2] <- NA
+   #  TALL_subtype_peds$`C12.2 (HOXA13)`[-pos2] <- NA
+   #  TALL_subtype_peds$`C12.3 (immature other)`[-pos2] <- NA
       
     } else {
       TALL_subtype_peds$`T-ALL sub-cluster candidate` <- gsub("C12.1 (MED12)", "", fixed = T, TALL_subtype_peds$`T-ALL sub-cluster candidate`)
@@ -914,9 +914,9 @@ allcatchr_1.1 <- function(Lineage = "B-ALL", Counts.file=NA, ID_class="symbol", 
       TALL_subtype_peds$`T-ALL sub-cluster candidate` <- gsub("C12.3 (immature other)", "", fixed = T, TALL_subtype_peds$`T-ALL sub-cluster candidate`)
     
      # this is new from here
-     TALL_subtype_peds$`C12.1 (MED12)` <- NA
-     TALL_subtype_peds$`C12.2 (HOXA13)` <- NA
-     TALL_subtype_peds$`C12.3 (immature other)` <- NA
+     #TALL_subtype_peds$`C12.1 (MED12)` <- NA
+     #TALL_subtype_peds$`C12.2 (HOXA13)` <- NA
+     #TALL_subtype_peds$`C12.3 (immature other)` <- NA
       
       
     }
