c77edb980cad943355570667e0ae4ba9322dfc7a
ThomasBeder <82496414+ThomasBeder@users.noreply.github.com>
2025-06-15 08:00:10 +0200

    Update allcatchr_1.1.R

--- R/allcatchr_1.1.R
+++ R/allcatchr_1.1.R
@@ -882,17 +882,17 @@ allcatchr_1.1 <- function(Lineage = "B-ALL", Counts.file=NA, ID_class="symbol", 
     TALL_subtype_peds$`T-ALL sub-cluster candidate`[-pos1] <- gsub("C1.1 (TAL1 αβ-like)", "", fixed = T, TALL_subtype_peds$`T-ALL sub-cluster candidate`[-pos1])
     TALL_subtype_peds$`T-ALL sub-cluster candidate`[-pos1] <- gsub("C1.2 (LMO2 γδ-like)", "", fixed = T, TALL_subtype_peds$`T-ALL sub-cluster candidate`[-pos1])
     
-     # this is new from here
-     #TALL_subtype_peds$`C1.1 (TAL1 αβ-like)`[-pos1] <- NA
-     #TALL_subtype_peds$`C1.2 (LMO2 γδ-like)`[-pos1] <- NA
+    ## set subcluster scores to NA in cases not predicted to be C1
+     TALL_subtype_peds$`C1.1 (TAL1 αβ-like)`[-pos1] <- NA
+     TALL_subtype_peds$`C1.2 (LMO2 γδ-like)`[-pos1] <- NA
     
     } else {
       TALL_subtype_peds$`T-ALL sub-cluster candidate` <- gsub("C1.1 (TAL1 αβ-like)", "", fixed = T, TALL_subtype_peds$`T-ALL sub-cluster candidate`)
       TALL_subtype_peds$`T-ALL sub-cluster candidate` <- gsub("C1.2 (LMO2 γδ-like)", "", fixed = T, TALL_subtype_peds$`T-ALL sub-cluster candidate`)
 
-     ## this is new from here
-     #TALL_subtype_peds$`C1.1 (TAL1 αβ-like)` <- NA
-     #TALL_subtype_peds$`C1.2 (LMO2 γδ-like)` <- NA
+     ## set subcluster scores to NA in cases not predicted to be C1
+     TALL_subtype_peds$`C1.1 (TAL1 αβ-like)` <- NA
+     TALL_subtype_peds$`C1.2 (LMO2 γδ-like)` <- NA
       
     }
     
@@ -903,20 +903,20 @@ allcatchr_1.1 <- function(Lineage = "B-ALL", Counts.file=NA, ID_class="symbol", 
     TALL_subtype_peds$`T-ALL sub-cluster candidate`[-pos2] <- gsub("C12.2 (HOXA13)", "", fixed = T, TALL_subtype_peds$`T-ALL sub-cluster candidate`[-pos2])
     TALL_subtype_peds$`T-ALL sub-cluster candidate`[-pos2] <- gsub("C12.3 (immature other)", "", fixed = T, TALL_subtype_peds$`T-ALL sub-cluster candidate`[-pos2])
   
-    # this is new from here
-   #  TALL_subtype_peds$`C12.1 (MED12)`[-pos2] <- NA
-   #  TALL_subtype_peds$`C12.2 (HOXA13)`[-pos2] <- NA
-   #  TALL_subtype_peds$`C12.3 (immature other)`[-pos2] <- NA
+    ## set subcluster scores to NA in cases not predicted to be C12
+     TALL_subtype_peds$`C12.1 (MED12)`[-pos2] <- NA
+     TALL_subtype_peds$`C12.2 (HOXA13)`[-pos2] <- NA
+     TALL_subtype_peds$`C12.3 (immature other)`[-pos2] <- NA
       
     } else {
       TALL_subtype_peds$`T-ALL sub-cluster candidate` <- gsub("C12.1 (MED12)", "", fixed = T, TALL_subtype_peds$`T-ALL sub-cluster candidate`)
       TALL_subtype_peds$`T-ALL sub-cluster candidate` <- gsub("C12.2 (HOXA13)", "", fixed = T, TALL_subtype_peds$`T-ALL sub-cluster candidate`)
       TALL_subtype_peds$`T-ALL sub-cluster candidate` <- gsub("C12.3 (immature other)", "", fixed = T, TALL_subtype_peds$`T-ALL sub-cluster candidate`)
     
-     # this is new from here
-     #TALL_subtype_peds$`C12.1 (MED12)` <- NA
-     #TALL_subtype_peds$`C12.2 (HOXA13)` <- NA
-     #TALL_subtype_peds$`C12.3 (immature other)` <- NA
+     ## set subcluster scores to NA in cases not predicted to be C12
+     TALL_subtype_peds$`C12.1 (MED12)` <- NA
+     TALL_subtype_peds$`C12.2 (HOXA13)` <- NA
+     TALL_subtype_peds$`C12.3 (immature other)` <- NA
       
       
     }
