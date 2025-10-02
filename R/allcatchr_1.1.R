ec8311f5d42eb255e66a21dc7cb87ab4cf184b19
ThomasBeder <82496414+ThomasBeder@users.noreply.github.com>
2025-10-02 12:56:18 +0200

    Update allcatchr_1.1.R

--- R/allcatchr_1.1.R
+++ R/allcatchr_1.1.R
@@ -1228,13 +1228,13 @@ df$cluster <- factor(df$cluster, levels = rev(subtype_order))
 
 plot <- ggplot2::ggplot(df, ggplot2::aes(y = cluster,
                x = score)) + 
-  ggplot2::ggtitle(paste0(output[i], "\nhigh-confidence: ", 
-                 paste0(output$`T-ALL main-cluster high-confidence`[i],
-                        output$`T-ALL sub-cluster high-confidence`[i],
+  ggplot2::ggtitle(paste0(output$sample[i], "\nhigh-confidence: ", 
+                 paste0(output$`T-ALL main-cluster high-confidence`[i],";",
+                        output$`T-ALL sub-cluster high-confidence`[i],";",
                         output$`T-ALL immature high-confidence`[i]),                        
                         "\ncandidate: ", 
-                 paste0(output$`T-ALL main-cluster candidate`[i],
-                        output$`T-ALL sub-cluster candidate`[i],
+                 paste0(output$`T-ALL main-cluster candidate`[i],";",
+                        output$`T-ALL sub-cluster candidate`[i],";",
                         output$`T-ALL immature candidate`[i]))
           )+ 
   ggplot2::geom_bar(stat = "identity") +
